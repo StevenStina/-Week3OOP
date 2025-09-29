25ad8a81c844d6b27cdfe9588f160ce2002e6743
Steven Stina <g00434645@atu.ie>
2025-09-29 10:28:44 +0100

    Add firstnumber + secondnumber

--- src/main/java/ie/atu/CalcApp.java
+++ src/main/java/ie/atu/CalcApp.java
@@ -1,4 +1,31 @@
 package ie.atu;
 
+import java.awt.*;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class CalcApp {
+
+    public static void main(String[] args) {
+        //Setup scanner
+        System.out.println("Please enter an operation");
+
+        //get operation
+        Scanner scan1 = new Scanner(System.in);
+        String operation = scan1.next();
+        System.out.println(operation);
+
+        Calculator myCalc = new Calculator();
+
+        System.out.println("Simple Calculator - Add Two Numbers");
+        System.out.println("Enter first number: ");
+        int firstnumber = scan1.nextInt();
+        System.out.println("Enter second number: ");
+        int secondnumber = scan1.nextInt();
+
+        int result = myCalc.add_numbers(firstnumber, secondnumber);
+        System.out.println("The total is " + result);
+
+        scan1.close();
+    }
 }
---
+++ src/main/java/ie/atu/Calculator.java
@@ -0,0 +1,17 @@
+package ie.atu;
+
+public class Calculator {
+
+    private String operation;
+
+    private int firstnumber;
+
+    private int secondnumber;
+
+
+    public int add_numbers(int firstnumber, int secondnumber)
+    {
+        int total =  firstnumber + secondnumber;
+        return total;
+    }
+}
